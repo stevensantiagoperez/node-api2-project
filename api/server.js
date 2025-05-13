5c08a043a24e1cbb8a9fc6eba2428d931b38019f
Steven Santiago-Perez <santiagoperezsteven@gmail.com>
2025-05-12 20:29:01 -0400

    wip

--- api/server.js
+++ api/server.js
@@ -1,2 +1,12 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require('express')
+
+const server = express()
+
+server.get('/api/users', (req, res) => {
+   res.json('users')
+})
+
+
+module.exports = server; 
