fff950dfb22adbb7d0cd1229da1d03ac9592564e
Steven Santiago-Perez <santiagoperezsteven@gmail.com>
2025-05-12 21:36:05 -0400

    get id working

--- api/server.js
+++ api/server.js
@@ -18,6 +18,26 @@ server.get('/api/posts', (req, res) => {
     })
    })
 })
+
+server.get('/api/posts/:id', (req, res)=> {
+    Post.findById(req.params.id)
+    .then(post => {
+        if(!post){
+            res.status(404).json({
+                message: 'The post with the specified ID does not exist'
+            })
+        }
+        res.json(post)
+    })
+    .catch(err => {
+    res.status(404).json({
+        message: 'The posts information could not be retrieved',
+        err: err.message,
+        stack: err. stack,
+    })
+   })
+    
+})
  
 
 module.exports = server; 
