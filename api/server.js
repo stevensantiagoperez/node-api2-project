7520ac6d6b48503bc380e897eea1a7bdeb8e9b8e
Steven Santiago-Perez <santiagoperezsteven@gmail.com>
2025-05-12 21:05:35 -0400

    get posts working

--- api/server.js
+++ api/server.js
@@ -1,12 +1,23 @@
 // implement your server here
 // require your posts router and connect it here
 const express = require('express')
+const Post = require('./posts/posts-model')
 
 const server = express()
 
-server.get('/api/users', (req, res) => {
-   res.json('users')
+server.get('/api/posts', (req, res) => {
+   Post.find()
+   .then(posts => {
+        res.json(posts)
+   })
+   .catch(err => {
+    res.status(500).json({
+        message: 'The posts information could not be retrieved',
+        err: err.message,
+        stack: err. stack,
+    })
+   })
 })
-
+ 
 
 module.exports = server; 
