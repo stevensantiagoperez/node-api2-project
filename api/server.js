d517858a60d12a98a17675e075cd960fb2102089
Steven Santiago-Perez <santiagoperezsteven@gmail.com>
2025-05-17 12:36:51 -0400

    comments working

--- api/server.js
+++ api/server.js
@@ -6,6 +6,30 @@ const Post = require('./posts/posts-model')
 const server = express()
 server.use(express.json( ))
 
+server.get('/api/posts/:id/comments', (req, res) => {
+    Post.findById(req.params.id)
+    .then(post => {
+        if(!post){
+            res.status(404).json({
+                message: 'The post with the specified ID does not exist'
+            })
+            res.json(post)
+
+        } else {
+            Post.findPostComments(req.params.id)
+            .then(comments => {
+                res.json(comments)
+            })
+            .catch(err => {
+                res.status(500).json({
+                message: 'The comments information could not be retrieved',
+                error: err.message,
+                })
+            })
+        }
+    })
+})
+
 server.put('/api/posts/:id', async (req, res) => {
     try{
         const possiblePost = await Post.findById(req.params.id)
